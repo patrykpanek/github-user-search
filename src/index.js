6db418b9ba439e95b1d1dcc526f6692e2c03acd4
Patryk Panek <patryyk221@gmail.com>
2022-06-10 19:00:48 +0200

    add Auth0, private route and refactor to router 6

--- src/index.js
+++ src/index.js
@@ -8,9 +8,15 @@ import { Auth0Provider } from '@auth0/auth0-react';
 
 ReactDOM.render(
   <React.StrictMode>
-    <GithubProvider>
-      <App />
-    </GithubProvider>
+    <Auth0Provider
+      domain="dev-to3mdnqd.us.auth0.com"
+      clientId="p4N3M3u2wQFk64lgOmTH1ZCMBnX1nzPK"
+      redirectUri={window.location.origin}
+    >
+      <GithubProvider>
+        <App />
+      </GithubProvider>
+    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
